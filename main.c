39b7a17f127f9dafc2845de6e6fb8ef71f6227d2
Alper Sapan <fbalperr33@gmail.com>
2021-08-29 09:50:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi, toplam;
+
+    while(sayi!=0){
+        printf("sayi giriniz\n");
+        scanf("%d", &sayi);
+
+        toplam=sayi+toplam;
+    }
+    printf("%d\n", toplam);
+    return 0;
+}
